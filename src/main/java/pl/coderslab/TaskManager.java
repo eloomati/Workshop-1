be6036b83f8af7eca4a7c59057bd4cc769758a7b
mhetko <mateusz.hetko@autopay.pl>
2025-04-08 23:59:21 +0200

    Poprawa dzialania flow programu na sterowany przez decyzje usera

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -12,20 +12,27 @@ import java.util.List;
 import java.util.Scanner;
 
 public class TaskManager {
+    static String[][] tasks;
 
-    public static void main(String[] args){
-        try{
+    public static void main(String[] args) {
+        try {
             printMenu();
-            String[][] tasks = loadTasks();
-            String[][] newTasks = addTask(tasks);
-            listTasks(newTasks);
-            String[][] removedTasks = removeTask(newTasks);
-            listTasks(removedTasks);
-        }catch (IOException e){
-            System.out.println(e.getMessage());}
+            tasks = loadTasks();
+            getUserDecision();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public static String[][] removeTask(String[][] copyOfTasksArray) throws IOException {
+
+
+    /**
+     * Removes a task from the tasks array.
+     *
+     * @throws IOException If an error occurs while reading input.
+     */
+    public static void removeTask() throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("remove");
         System.out.println("Please select number to remove");
@@ -33,29 +40,34 @@ public class TaskManager {
         while (true) {
             if (scanner.hasNextInt()) {
                 int numberOfTask = scanner.nextInt();
-                if (numberOfTask >= 0 && numberOfTask < copyOfTasksArray.length) {
-                    String[][] arrayWithDeletedTask = ArrayUtils.remove(copyOfTasksArray, numberOfTask);
-                    copyOfTasksArray = null; // Zwolnienie pamięci
+                if (numberOfTask >= 0 && numberOfTask < tasks.length) {
+                    tasks = ArrayUtils.remove(tasks, numberOfTask);
                     System.out.println("Value was successfully deleted.");
-                    return arrayWithDeletedTask;
+                    break;
                 } else {
-                    System.out.println("Incorrect argument pass. Please give a number greater or equal 0 in range of tasks.");
+                    System.out.println(numberOfTask + "\nIncorrect argument pass. Please give a number greater or equal 0 in range of tasks.");
                 }
             } else {
-                System.out.println("Incorrect argument pass. Please give a number.");
-                scanner.next(); // Konsumowanie niepoprawnego wejścia
+                String badInput = scanner.next();
+                System.out.println(badInput + "\nIncorrect argument pass. Please give a number.");
             }
         }
     }
 
-    public static void listTasks(String[][] tasks){
-       System.out.println("list");
-        for(int i = 0; i < tasks.length; i++){
-            System.out.println(i + " : "+ tasks[i][0] + " " + tasks[i][1] + " " + tasks[i][2]);
+    /**
+     * Prints the tasks to the console.
+     */
+    public static void listTasks() {
+        System.out.println("list");
+        for (int i = 0; i < tasks.length; i++) {
+            System.out.println(i + " : " + tasks[i][0] + " " + tasks[i][1] + " " + tasks[i][2]);
         }
     }
 
-    public static String[][] addTask(String[][] tasks){
+    /**
+     * Adds a new task to the tasks array.
+     */
+    public static void addTask() {
         // Collect data from the user
         System.out.println("add");
         Scanner scanner = new Scanner(System.in);
@@ -67,34 +79,40 @@ public class TaskManager {
         String taskImportant = scanner.nextLine();
 
         // Create a new array with one additional row
-        String[][] copyOfTasksArray = Arrays.copyOf(tasks, tasks.length + 1);
-        copyOfTasksArray[tasks.length] = new String[]{taskDescription, taskDueDate, taskImportant};
+        tasks = Arrays.copyOf(tasks, tasks.length + 1);
+        tasks[tasks.length - 1] = new String[]{taskDescription, taskDueDate, taskImportant};
 
-        return copyOfTasksArray;
+        System.out.println("Task added successfully.");
     }
 
-    public static void getUserDecision(String[][] tasks){
+    public static void getUserDecision() {
         Scanner scanner = new Scanner(System.in);
-        String userDecision = scanner.nextLine();
-        switch (userDecision) {
-            case "add":
-                System.out.println("Adding a task...");
-                addTask(tasks);
-                break;
-            case "remove":
-                System.out.println("Removing a task...");
-               // romeveTask();
-                break;
-            case "list":
-                System.out.println("Listing a task...");
-                //listTasks();
-                break;
-            case "exit":
-                System.out.println("Exiting the program.");
-                break;
-            default:
-                System.out.println("Please select a correct option.");
+        while (scanner.hasNextLine()) {
+            String userDecision = scanner.nextLine();
+            switch (userDecision) {
+                case "add":
+                    addTask();
+                    break;
+                case "remove":
+                    try {
+                        removeTask();
+                        break;
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "list":
+                    listTasks();
+                    break;
+                case "exit":
+                    System.out.println("Exiting the program.");
+                    break;
+                default:
+                    System.out.println("Please select a correct option.");
+                    printMenu();
+            }
         }
+        printMenu();
     }
 
     /**
@@ -110,7 +128,7 @@ public class TaskManager {
         try {
             Scanner scanner = new Scanner(path);
 
-            while(scanner.hasNextLine()){
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] task = line.split(",");
                 taskList.add(task);
@@ -118,14 +136,13 @@ public class TaskManager {
         } catch (IOException e) {
             throw new IOException("Error during reading file: " + path, e);
         }
-
         return taskList.toArray(new String[0][]);
     }
 
     /**
      * Prints the menu options to the console.
      */
-    public static void printMenu(){
+    public static void printMenu() {
         String[] menuOptions = {
                 "add",
                 "remove",
@@ -134,7 +151,7 @@ public class TaskManager {
         };
         System.out.println(ConsoleColors.BLUE + "Please select an option:"
                 + ConsoleColors.RESET);
-        for (String option: menuOptions){
+        for (String option : menuOptions) {
             System.out.println(option);
         }
     }
